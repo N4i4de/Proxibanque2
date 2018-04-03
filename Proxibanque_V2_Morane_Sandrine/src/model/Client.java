ea921d75da6a4bdc0e3e19f6cde09f329fd5b97b
morane <morane.musa@gmail.com>
2018-04-03 13:42:43 +0200

    ajout constructeur

--- Proxibanque_V2_Morane_Sandrine/src/model/Client.java
+++ Proxibanque_V2_Morane_Sandrine/src/model/Client.java
@@ -14,5 +14,18 @@ public class Client {
 	private String ville;
 	private String telephone;
 	private String email;
+	
+	public Client(int id, String nom, String prenom, String adresse, String codePostal, String ville, String telephone,
+			String email) {
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.adresse = adresse;
+		this.codePostal = codePostal;
+		this.ville = ville;
+		this.telephone = telephone;
+		this.email = email;
+	}
 
+	
 }
