2d91548548abc7381f530c1340c35716d5ad876b
N4i4de <sandrine.lementec>
2018-04-04 11:26:40 +0200

    ajout servlet tri boulot

--- Proxibanque_V2_Morane_Sandrine/src/presentation/ServletAfficherListeClient.java
+++ Proxibanque_V2_Morane_Sandrine/src/presentation/ServletAfficherListeClient.java
@@ -1,11 +1,21 @@
 package presentation;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.Client;
+import model.Conseiller;
+import service.GestionClient;
+import service.InterfaceGestionClient;
+import service.InterfaceGestionConseiller;
 
 /**
  * Servlet implementation class ServletAfficherListeClient
@@ -35,6 +45,14 @@ public class ServletAfficherListeClient extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO affichage des listes clients
+		HttpSession session = request.getSession();
+		Conseiller conseiller = (Conseiller)session.getAttribute("conseillerconnecte");
+		InterfaceGestionClient gestionclient = new GestionClient();
+		List<Client> listclient=gestionclient.listerClient(conseiller.getIdconseiller());
+        request.setAttribute("listclient", listclient);
+        RequestDispatcher dispatcher;
+        dispatcher=request.getRequestDispatcher("listeClients.jsp");
+        dispatcher.forward(request, response);
 		//doGet(request, response);
 	}
 
